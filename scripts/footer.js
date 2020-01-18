7024f77e46b273c68f6b017fcb0292f739191fd7
Lucas fransman <lucas.fransman@gmail.com>
2020-01-18 23:20:52 +0200

    first commit.

---
+++ scripts/footer.js
@@ -0,0 +1,20 @@
+// Size of browser viewport.
+
+
+$(window).resize(sizeTriangle());
+
+
+function sizeTriangle(){
+    let x = $(window).width();
+    alert(x);
+    console.log("done")
+}
+
+//$(window).height();
+//$(window).width();
+
+// Size of HTML document (same as pageHeight/pageWidth in screenshot).
+//$(document).height();
+//$(document).width();
+
+//$("#right-triangle").attr("triangle-right");
